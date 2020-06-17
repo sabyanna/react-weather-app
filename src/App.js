b5ece4d05c59633867fc4548760169b0ca30c35d
sabyanna <anna.sabransky@cx-ray.com>
2020-06-17 12:13:35 +0200

    aap div tree fixed

--- src/App.js
+++ src/App.js
@@ -12,21 +12,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Weather App</h1>
-        <div className="container">
-          <div className="row justify-content-md-center" style={{marginBottom: '10px'}}>
-            <div>
-              <NearByWeatherButton setLocationsWeatherData={ setLocationsWeatherData } />
-            </div>
-            <div>
-              <RadiusInput setLocationsWeatherData={ setLocationsWeatherData } />
-            </div>
-          </div>
-          <Search setLocationsWeatherData={ setLocationsWeatherData } />
-          { locationsWeatherData &&
-            <WeatherTable nearbyWeather={ locationsWeatherData }/>
-          }
-        </div>
       </header>
+      <div className="container">
+        <div className="row justify-content-md-center" style={{marginBottom: '10px'}}>
+          <div>
+            <NearByWeatherButton setLocationsWeatherData={ setLocationsWeatherData } />
+          </div>
+          <div>
+            <RadiusInput setLocationsWeatherData={ setLocationsWeatherData } />
+          </div>
+        </div>
+        <Search setLocationsWeatherData={ setLocationsWeatherData } />
+        { locationsWeatherData &&
+          <WeatherTable nearbyWeather={ locationsWeatherData }/>
+        }
+      </div>
     </div>
   );
 }
